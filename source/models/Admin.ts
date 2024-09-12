ff5f354ca96c7a6bae2a0b6aa67465fb2ba1be12
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2024-09-12 19:46:47 +0100

    Add MongoDB models for user,admin.

---
+++ source/models/Admin.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IAdmin extends Document {
+  email: string;
+  password: string;
+};
+
+const adminSchema: Schema<IAdmin> = new Schema<IAdmin>(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Please enter your email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "Please add a password "],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Admin = mongoose.model<IAdmin>("admin", adminSchema);
+export default Admin;
---
+++ source/models/User.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  businessType: string;
+};
+
+const userSchema: Schema<IUser> = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, "Please enter your firstName"],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      required: [true, "Please enter your secondName"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Please enter your email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "Please add a password "],
+    },
+    businessType: {
+        type: String,
+        required: [true, "Please enter a business Name"],
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model<IUser>("user", userSchema);
+export default User;
